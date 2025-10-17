05d7e6bb6a2f39c36aa5f35c2bb3ccca8bf542cd
TheOttoZone <ako415@hotmail.com>
2025-10-16 23:59:03 -0700

    add unfinished assignment 2

---
+++ assignment02/Mapper.c
@@ -0,0 +1,26 @@
+﻿#include "Mapper.h"
+#include <stdio.h>
+
+void OutputAsHexCharacters(unsigned int value)
+{
+	unsigned int mask = 0x000000F;
+    unsigned int nibble = 0;
+    unsigned int returnValue = 0;
+    char array[8];
+
+    for(int i = 1; i < 9; i++){
+        nibble = value & mask;
+        mask = mask << 4;
+        nibble = nibble >> ((i * 4) - 4);
+        printf("%c\n", MapNibbleToHexCharacter(nibble));
+        //returnValue = returnValue | nibble; 
+        //printf("0x%08X\n", returnValue); 
+    }
+
+}
+
+unsigned char MapNibbleToHexCharacter(unsigned int value)
+{
+    char hexArray[] = "0123456789ABCDEF";
+    return hexArray[value];
+}
---
+++ assignment02/Program.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include "Mapper.h"
+
+int main() 
+{
+   
+	//Input
+	unsigned int value = 0;
+	
+	printf("Enter an unsigned integer value: ");
+	int returnValue = scanf("%u", &value);
+
+	//Process & Output
+	OutputAsHexCharacters(value);
+
+	printf("Press the enter key to continue...\n");
+
+	return 0;
+}
